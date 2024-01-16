a139befc62a8a09d09993009769b733091567b54
julioCarrilloDEV <jc.carrillodev@gmail.com>
2024-01-16 17:05:12 -0300

    Ajuste de bugs na função delete

--- public/js/exclusao.js
+++ public/js/exclusao.js
@@ -17,15 +17,19 @@ function excluirConsulta(consultaId) {
     fetch('/' + consultaId, {
         method: 'DELETE'
     })
-    .then(response => {
-        if (response.ok) {
-            // Exclusão bem-sucedida, recarregue a página ou atualize a tabela
-            location.reload();
-        } else {
-            console.error('Erro ao excluir consulta:', response.statusText);
-        }
+    .then(response => response.json())
+    .then(data => {
+        console.log('Consulta excluída:', data);
+        
     })
     .catch(error => {
         console.error('Erro ao excluir consulta:', error);
     });
 }
+
+// Adicione um listener para o evento hidden.bs.modal
+var confirmarExclusaoModal = document.getElementById('confirmarExclusaoModal');
+confirmarExclusaoModal.addEventListener('hidden.bs.modal', function () {
+    // Recarregar a página após o modal ser fechado
+    window.location.reload(true);
+});
