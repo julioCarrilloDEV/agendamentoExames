1dce9e72645a1538ff31300d61909d625dfc4d3f
julioCarrilloDEV <jc.carrillodev@gmail.com>
2024-01-18 14:27:06 -0300

    Correçaõ de bugs nos modais de criação e alteração de cadastro.

--- public/js/criacao.js
+++ public/js/criacao.js
@@ -1,11 +1,14 @@
-function enviarFormulario() {
+function enviarFormularioAdd() {
     // Obtenha os dados do formulário
-    var formData = new FormData(document.getElementById('seuFormularioId'));
+    var formData = new FormData(document.getElementById('formularioAdd'));
 
     // Enviar uma requisição POST usando AJAX
     fetch('/', {
         method: 'POST',
-        body: formData
+        headers: {
+            'Content-Type': 'application/json' // Indique que estamos enviando JSON
+        },
+        body: JSON.stringify(formData) // Converta o objeto JSON para uma string JSON
     })
     .then(response => response.json())
     .then(data => {
@@ -13,7 +16,7 @@ function enviarFormulario() {
         // Fechar o modal após o envio do formulário
         var modal = new bootstrap.Modal(document.getElementById('novoCadastro'));
         modal.hide();
-        
+
         // Recarregar a página após o envio do formulário
         window.location.reload(true);
     })
